4ec6a64fefceb3c49a928bc9752ceabbe888f04f
Amitesh Anand <aamitesh7@gmail.com>
2022-08-23 15:57:28 +0530

    add input fields to pass test

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,21 @@
 import "./App.css";
 
 function Login() {
-  return <div>This is the Form</div>;
+  return (
+    <div>
+      <div>This is the Login Form</div>
+      <div>
+        <div className="section email">
+          <label for="email">Email Address</label>
+          <input id="email" type="email" name="email"></input>
+        </div>
+        <div className=" section password">
+          <label for="password">Password</label>
+          <input id="password" type="password" name="password"></input>
+        </div>
+      </div>
+      <button type="submit">Submit</button>
+    </div>
+  );
 }
 export default Login;
