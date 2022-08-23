55b8b1be53a8179bbf8e15e5ed7652614d498ab5
Amitesh Anand <aamitesh7@gmail.com>
2022-08-23 15:56:52 +0530

    add initial input value tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-
-// test("renders learn react link", () => {
-//   render(<App />);
-//   // const linkElement = screen.getByText(/Test React App/i);
-//   // expect(linkElement).toBeInTheDocument();
-// });
-
-it("", () => {});
+describe("check initial value of inputs", () => {
+  it("email input shoulld be initially empty", () => {
+    render(<App />);
+    const emailInputElement = screen.getByRole("textbox");
+    console.log(emailInputElement);
+    expect(emailInputElement.value).toBe("");
+  });
+  it("password input shoulld be initially empty", () => {
+    render(<App />);
+    const passwordInputElement = screen.getByLabelText(/password/i);
+    console.log(passwordInputElement);
+    expect(passwordInputElement.value).toBe("");
+  });
+});
