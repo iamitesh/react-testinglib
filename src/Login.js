7d33f5fe4c47f89900a30ee5686c7ee435c615d0
Amitesh Anand <aamitesh7@gmail.com>
2022-08-23 17:04:54 +0530

    twoWyBinding input elements

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
 function Login() {
+  const [loginInput, setLoginInput] = useState({
+    email: "",
+    password: "",
+  });
+  const changeHandler = (e) => {
+    setLoginInput({ ...loginInput, [e.target.name]: e.target.value });
+  };
   return (
     <div>
       <div>This is the Login Form</div>
-      <div>
-        <div className="section email">
-          <label for="email">Email Address</label>
-          <input id="email" type="email" name="email"></input>
+      <form>
+        <div className="emailWrapper">
+          <div className="section email">
+            <label for="email">Email Address</label>
+            <input id="email" type="email" name="email" required></input>
+          </div>
+          <div className="email-err err">Invalid Email</div>
         </div>
-        <div className=" section password">
-          <label for="password">Password</label>
-          <input id="password" type="password" name="password"></input>
+        <div className="passwordWrapper">
+          <div className=" section password">
+            <label for="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              value={loginInput.password}
+              onChange={changeHandler}
+              required
+            ></input>
+          </div>
+          <div className="password-err err">Invalid Password</div>
         </div>
-      </div>
-      <button type="submit">Submit</button>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
