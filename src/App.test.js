a589b29cfbef33988e9c08a804225820423e5665
Amitesh Anand <aamitesh7@gmail.com>
2022-08-23 16:12:06 +0530

    add Tcase for input validity

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import App from "./App";
 describe("check initial value of inputs", () => {
   it("email input shoulld be initially empty", () => {
@@ -12,5 +14,21 @@ describe("check initial value of inputs", () => {
     const passwordInputElement = screen.getByLabelText(/password/i);
     console.log(passwordInputElement);
     expect(passwordInputElement.value).toBe("");
+  });
+});
+describe("should be able to type in inputs", () => {
+  it("should be able to type email", () => {
+    render(<App />);
+    const emailInputElement = screen.getByRole("textbox", {
+      name: /email/i,
+    });
+    userEvent.type(emailInputElement, "amit@gmail.com");
+    expect(emailInputElement.value).toBe("amit@gmail.com");
+  });
+  it("should be able to type password", () => {
+    render(<App />);
+    const passwordInputElement = screen.getByLabelText(/password/i);
+    userEvent.type(passwordInputElement, "12345");
+    expect(passwordInputElement.value).toBe("12345");
   });
 });
